2f94e2c88bafdfec037e199cd843359fdf802b31
maisaa <maisaa.alkhedr@gmail.com>
2017-08-26 09:58:34 +0300

    finish the array-extensions

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,13 @@
-// your code here
+var extendArray function(arr){
+    
+    this.first = function(value){
+    	var shifted = arr.shift();
+    	return shifted;
+      
+    };
+    this.last = function() {
+      var unshifted = arr.unshift([arr.length-1]);
+      return unshifted;
+    };
+
+}
