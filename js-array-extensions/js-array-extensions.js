7a72fbc416c3ec9a3ab71f790f07ea03c343ed86
maisaa <maisaa.alkhedr@gmail.com>
2017-09-03 09:46:57 +0300

    Assesment modifying by maisaa

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,13 +1,6 @@
-var extendArray function(arr){
-    
-    this.first = function(value){
-    	var shifted = arr.shift();
-    	return shifted;
-      
-    };
-    this.last = function() {
-      var unshifted = arr.unshift([arr.length-1]);
-      return unshifted;
-    };
-
+Array.prototype.first = function(){
+	return arr[0];
+}
+Array.prototype.last = function(){
+	return arr[arr.length-1];
 }
